f8ac20099c8b71173a4a5cf677af92eec1776bfa
shahrulnizam <53547253+Arexxa@users.noreply.github.com>
2021-01-27 12:39:55 +0800

    add tag (#8)
    * add tag
    
    * sign in button updated

---
+++ src/models/Taging.js
@@ -0,0 +1,19 @@
+import { Model } from '@vuex-orm/core'
+import Repository from './Repository'
+import Note from './Note'
+
+export default class Taging extends Model {
+
+  static entity = 'taging'
+
+  static primaryKey = 'id'
+
+  static fields () {
+    return {
+      id: this.attr(null),
+      repositoryId: this.attr(''),
+      tagIds: this.attr('')
+    }
+
+  }
+}
