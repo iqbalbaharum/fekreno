071baa658c04a67100ef60f1c17dc89ba345c8b3
piji <syafiqzi98@gmail.com>
2021-02-07 19:03:57 +0800

    Update TermCondition.vue

--- src/pages/User/TermCondition.vue
+++ src/pages/User/TermCondition.vue
@@ -11,7 +11,23 @@
         <div class="q-mb-xl">
           <p class="text-h3"> Term and Condition</p>
           <div>
-            Effective on 4 February 2021
+            <p>PLEASE NOTE THAT YOUR USE OF AND ACCESS TO OUR SERVICES (DEFINED BELOW) 
+            ARE SUBJECT TO THE FOLLOWING TERMS. IF YOU DO NOT AGREE TO ALL OF THE FOLLOWING, 
+            YOU MAY NOT USE OR ACCESS THE SERVICES IN ANY MANNER.</p>
+            <p class="text-h5"> Term of Use </p>
+            <p> Effective on 39 February 2021 </p>
+            <p> Welcome to Kaggle. Please read on to learn the rules and restrictions that govern your use of our website(s), 
+            products, services and applications (the “Services”). 
+            If you have any questions, comments, or concerns regarding these Terms or the Services, please contact us. 
+            These Terms of Use (the “Terms”) are a binding contract between you and Kaggle Inc. (“Kaggle,” “we” and “us”). 
+            You must agree to and accept all of the Terms, or you don’t have the right to use the Services. 
+            Your use of the Services in any way means that you agree to all of these Terms, and these Terms will remain in effect while you use the Services. 
+            These Terms include the provisions in this document, as well as those in the Privacy Policy and Copyright Dispute Policy. 
+            In these Terms, the words “include” or “including” mean “including but not limited to”, 
+            and examples are for illustration purposes and are not limiting. </p>
+            <p class="text-h6"> Will these Terms ever change? </p>
+            <p> We are constantly trying to improve our Services, so these Terms may need to change along with the Services. We reserve the right to change the Terms at any time, 
+            but if we do, we will bring it to your attention by placing a notice on the www.kaggle.com website, by sending you an email, or by some other means. </p>
           </div>
         </div>
       </div> 
