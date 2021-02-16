ab3fe439a73a78d4dca2ead6ed361ae9b788a172
balqis93 <32974307+balqis93@users.noreply.github.com>
2021-02-16 12:35:18 +0800

    Added terms and condition page (#54)
    * added terms of use
    
    * whitelist /policy /terms
    
    Co-authored-by: balqisramlee93 <balqisramlee93@hotmail.com>
    Co-authored-by: iqbalbaharum <>

---
+++ src/pages/User/Terms.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page>
+    <div class="q-pa-md q-gutter-sm">
+      <q-breadcrumbs>
+        <q-breadcrumbs-el label="Home" to="/" />
+        <q-breadcrumbs-el label="Terms" />
+      </q-breadcrumbs>
+    </div>
+    <div class="row q-pa-xl">
+      <div class="col-8">
+        <div class="q-mb-xl">
+          <p class="text-h3">Terms of Use</p>
+          <div>
+            Effective on 4 February 2021
+          </div>
+        </div>
+      </div> 
+    </div>
+  </q-page>
+</template>
