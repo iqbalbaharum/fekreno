08529db7d11562c827f68da60b0188bd0b734542
nasuhasri <63119811+nasuhasri@users.noreply.github.com>
2021-03-22 21:37:41 +0800

    create a page for non-loggedin user (#71)
    * create a page for non-loggedin user
    
    If public user wants to go to '/',  system will redirect them to '/pubic'
    
    * Update routing.js
    
    Change path if user input the path that not matched to the exists path

---
+++ src/pages/Website/Public.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="fullscreen row">
+    <div class="col">
+    </div>
+    <div class="col bg-primary">
+    </div>
+    <div class="fullscreen flex flex-center">
+      <q-card class="q-pa-md" style="width:400px;">
+        <div class="text-weight-bold text-center q-pb-lg q-gutter-sm">
+          <q-img class="q-mb-md" src="~assets/krenovator.png"/>
+          <div class="text-h4 text-primary">Hello!</div>
+          <div class="text-caption">
+            Page will coming soon... Psst be part of us by click the register button.
+          </div>
+        </div>
+
+        <div class="flex flex-center">
+          <q-btn to="/login" color="primary" label="Login" />          
+        </div>
+
+        <div class="q-gutter-sm flex flex-center">
+          <q-btn
+            color="primary"
+            text-color="primary"
+            flat
+            to="/register"
+            label="Not register? Create account"
+            no-caps />
+        </div>
+      </q-card>
+    </div>
+  </div>
+</template>
